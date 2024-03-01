d0c59ac6ea4c108e04a40b76834f7c45b1ec86c5
Viet Le cuongle1998@hotmail.com <cuongle1998@hotmail.com>
2024-03-01 15:49:57 +1100

    return is cleaner and now has headers

--- petrol_scrape.py
+++ petrol_scrape.py
@@ -1,19 +1,23 @@
 from bs4 import BeautifulSoup
 import requests
 
-
+# I could use lat and long as parameter?¿
+    # Not very user friendly
 def petrol_scrape() -> str:
     url = 'https://petrolspy.com.au/map/latlng/-37.792503416413055/144.82380823806795'
     page = requests.get(url)
     soup = BeautifulSoup(page.text, 'html.parser')
+    header = soup.find('h3' , id='main-station-list-header').text
     box = soup.find('table', id='main-station-list-table')
     rows = box.find_all('td')
-    # print(rows[1])
 
-    returnText = ''
+    returnText = f'{header}\n\n'
 
     for row in range(0, len(rows) - 1, 2):
-        returnText += rows[row].text
-        returnText += f'{rows[row + 1].text}\n'
+        returnText += f'- {rows[row].text}\n{rows[row + 1].text}\n\n'
+
+    # print(returnText)
 
     return returnText
+
+# petrol_scrape()
