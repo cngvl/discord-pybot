be9b88c77993989e95ad8b5d3b0755002cb7d69c
Viet Le cuongle1998@hotmail.com <cuongle1998@hotmail.com>
2024-03-01 23:40:34 +1100

    added function to retrieve lat and long coords

--- petrol_scrape.py
+++ petrol_scrape.py
@@ -1,8 +1,28 @@
 from bs4 import BeautifulSoup
+from dotenv import load_dotenv
 import requests
+import os
 
 # I could use lat and long as parameter?¿
     # Not very user friendly
+
+load_dotenv()
+WEATHER_TOKEN = os.getenv('OPENWEATHER_TOKEN')
+
+def get_lat_long(postCode: str) -> list:
+    # url = f'http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid={WEATHER_TOKEN}'
+    url = f'https://api.openweathermap.org/geo/1.0/zip?zip={postCode},AU&appid={WEATHER_TOKEN}'
+    page = requests.get(url).json()
+    # print(page.json()['lat'])
+    # print(page.json()['lon'])
+
+    latlong = [page['lat'], page['lon']]
+    print(latlong)
+
+    return latlong
+
+get_lat_long('3020')
+
 def petrol_scrape() -> str:
     url = 'https://petrolspy.com.au/map/latlng/-37.792503416413055/144.82380823806795'
     page = requests.get(url)
