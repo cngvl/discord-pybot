79eae6041da877f723278d994ed872f98934788b
Viet Le cuongle1998@hotmail.com <cuongle1998@hotmail.com>
2024-03-13 23:50:59 +1100

    storing test dummy

--- dummy.py
+++ dummy.py
@@ -1,5 +1,15 @@
 import csv
 from petrol_scrape import petrol_scrape, get_lat_long
+from datetime import datetime
+# import datetime
 
-coords = get_lat_long('$petty 3025')
-print(coords)
+# coords = get_lat_long('$petty 3025')
+# print(coords)
+# petty = petrol_scrape('$petty 3020')
+# print(petty)
+
+# print('x')
+
+print(datetime.utcnow().strftime('%Y-%m-%d'))
+# d.strftime("%A %d. %B %Y")
+# 'Monday 11. March 2002'
